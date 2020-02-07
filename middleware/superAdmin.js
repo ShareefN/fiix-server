16a8dd95f1af9d5451b994501d41bdf5cc1b9bb5
SHAREEF NUSEIBEH <s.nuseibeh@eonaligner.com>
2020-02-07 15:29:45 +0200

    superAdmin bug needs improvment

--- middleware/superAdmin.js
+++ middleware/superAdmin.js
@@ -1,5 +1,4 @@
 superAdmin = (req, res, next) => {
-  console.log(req.user.admin.isSuperAdmin)
   if(req.user.admin.isSuperAdmin == false)
   return res.status(403).send({message: 'Access Denied.'})
   next()
