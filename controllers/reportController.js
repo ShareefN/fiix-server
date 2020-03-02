80f12b5ab0cff95584a258bed47ce8b21328f2bd
SHAREEF NUSEIBEH <s.nuseibeh@eonaligner.com>
2020-03-02 20:28:16 +0200

    update reports added

--- controllers/reportController.js
+++ controllers/reportController.js
@@ -9,14 +9,23 @@ router.post('/report/:id', [authToken], async (req, res) => {
   if(!user) return res.status(404).send({message: "Not found"})
 
   const report = {
-    username: user.dataValues.username,
-    number: user.dataValues.number,
+    username: user.username,
+    number: user.number,
     report: req.body.report
   }
 
   await reports.create(report)
   .then(() => res.status(200).send({message: 'success'}))
   .catch(err => res.status(500).send({error: err.messgae}))
+})
+
+router.put('/update/report/:reportId', [authToken], async (req, res) => {
+  const report = await reports.findOne({where: {id: req.params.reportId}})
+  if(!report) return res.status(404).send({message: 'Not Found'})
+
+  await reports.update(req.body, {where: {id: req.params.reportId}})
+  .then(() => res.status(200).send({message: 'success'}))
+  .catch(err => res.status(500).send({error: err.message}))
 })
 
 module.exports = router
--- models/reports.js
+++ models/reports.js
@@ -4,6 +4,7 @@ module.exports = (sequelize, DataTypes) => {
     username: DataTypes.STRING,
     number: DataTypes.STRING,
     report: DataTypes.STRING,
+    status: DataTypes.STRING,
     notes: DataTypes.STRING
   }, {});
   reports.associate = function(models) {
