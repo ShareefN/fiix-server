a8a0b013c41a035e30eac867907a161700c52940
SHAREEF NUSEIBEH <s.nuseibeh@eonaligner.com>
2020-04-05 21:16:25 +0300

    23

--- index.js
+++ index.js
@@ -21,7 +21,10 @@ if (!config.get("jwtPrivateKey")) {
 
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
-  res.header("Origin, X-Requested-With, Content-Type, Accept");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
   next();
 });
 
