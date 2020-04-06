e612348a5c5e4cbde77a6debb38aa1c508b36956
SHAREEF NUSEIBEH <s.nuseibeh@eonaligner.com>
2020-04-06 20:44:40 +0300

    allow CORS

--- index.js
+++ index.js
@@ -19,6 +19,15 @@ if (!config.get("jwtPrivateKey")) {
   throw new Error("Error: jwtPrivateKey is not defined.");
 }
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+
 require("./routes/index")(app);
 require("./config/prod")(app);
 
