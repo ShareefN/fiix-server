10615dda7987a7de400c290089049d8b2e644a13
SHAREEF NUSEIBEH <s.nuseibeh@eonaligner.com>
2020-04-10 18:43:58 +0300

    alowing *

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
+  res.header("Access-Control-Allow-Origin", "*");
   next();
 });
 
