e96292343a8a08f585a86109717af6bdfaca4394
SHAREEF NUSEIBEH <s.nuseibeh@eonaligner.com>
2020-04-10 18:38:20 +0300

    allwoing localhost

--- index.js
+++ index.js
@@ -21,21 +21,12 @@ if (!config.get("jwtPrivateKey")) {
   throw new Error("Error: jwtPrivateKey is not defined.");
 }
 
-app.use(cors())
+app.use(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Credentials", true);
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header(
-    "Access-Control-Allow-Headers",
-    "X-Requested-With, X-HTTP-Method-Override, Content-Type, Accept"
-  );
+  res.header("Access-Control-Allow-Origin", "http://127.0.0.1:3000");
   next();
 });
 
