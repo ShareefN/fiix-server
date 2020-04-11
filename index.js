198eae52aa4a2b9ebd47deda0fa9b0f227bb0a21
SHAREEF NUSEIBEH <s.nuseibeh@eonaligner.com>
2020-04-11 18:05:00 +0300

    allow cors

--- index.js
+++ index.js
@@ -26,7 +26,8 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Content-Type: application/json");
+  res.header("Access-Control-Allow-Origin: *");
   next();
 });
 
