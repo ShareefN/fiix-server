caab9b65e2eedc22e09e2952604e5de8060f25e4
SHAREEF NUSEIBEH <s.nuseibeh@eonaligner.com>
2020-05-19 23:24:07 +0300

    forgot password testing

--- controllers/forgotPassword.js
+++ controllers/forgotPassword.js
@@ -1,32 +1,19 @@
 const express = require("express");
 const router = express.Router();
-const { users } = require("../models");
-const nodeMailer = require("nodemailer");
+// const { users } = require("../models");
+const sgMail = require("@sendgrid/mail");
+
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 router.post("/forgotpassword", async (req, res) => {
-  const user = await users.findOne({ where: { email: req.body.email } });
-  if (!user) return res.status(404).send({ message: "Not found" });
-
-  const transporter = await nodeMailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: `s.nuseibeh@eonaligner.com`,
-      pass: `b5k126d7`
-    }
-  });
-
-  const mailOptions = {
-    from: "s.nuseibeh@eonaligner.com",
-    to: `s.nuseibeh@eonaligner.com`,
-    subject: "Link to reset password",
-    text: "This link will help you reset your password"
+  const msg = {
+    to: "shareefnuseibeh1@yahoo.com",
+    from: "fiixcareers@gmail.com",
+    subject: "Sending with Twilio SendGrid is Fun",
+    text: "and easy to do anywhere, even with Node.js",
+    html: "<strong>and easy to do anywhere, even with Node.js</strong>"
   };
-
-  transporter.sendMail(mailOptions, (err, response) => {
-    if (err) return res.status(500).send({ error: err });
-
-    return res.status(200).send({ message: "success" });
-  });
+  sgMail.send(msg);
 });
 
 module.exports = router;
